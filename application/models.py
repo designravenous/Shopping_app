e57e9bbc7e171ec0ad0c7cf07e71994debd42fe7
Peter Holgersson <peterholgersson@Peters-iMac.local>
2019-02-10 15:37:38 +0100

    install flask_login initialized login = LoginManager(app), login.user_loader

--- application/models.py
+++ application/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from application import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from application import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -11,7 +13,7 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
-
+    
     def set_hash(self, password):
         self.password_hash = generate_password_hash(password)
     
@@ -28,4 +30,11 @@ class ShoppingItems(db.Model):
 
     def __repr__(self):
         return '<Item {}, Quantity {}, added to chart? {}>'.format(self.item, self.quantity, self.added_to_chart)
+
+#function is needed to keep track of logged in user, the ID that is passed in the function is a string.  
+@app.login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
 
