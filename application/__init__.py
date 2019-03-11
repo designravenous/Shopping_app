49159ba0243e9a157281010dc8bea629dfc0f9ad
Peter Holgersson <petersholgersson@gmail.com>
2019-03-11 21:52:38 +0100

    start with flask_mail config etc

--- application/__init__.py
+++ application/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
+from flask_mail import Mail
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
@@ -13,6 +14,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 login = LoginManager(app)
+mail = Mail(app)
 #To let flask-login know what function handles logins:
 login.login_view = 'login'
 
--- config.py
+++ config.py
@@ -7,3 +7,10 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-never-find-out'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = '*******@gmail.com'
+    MAIL_PASSWORD = '**********'
+    ADMINS = ['*********@gmail.com']
+
